796da65057b680b1727b2802dbd1c3d7e18c25d3
Aleksandra Korotkova <korotkova.a34@rwb.ru>
2025-04-21 11:06:29 +0300

    added task 1

---
+++ data-types/homework_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"testing"
+	"unsafe"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// go test -v homework_test.go
+
+func ToLittleEndian(number uint32) uint32 {
+	var newNumber uint32
+	size := unsafe.Sizeof(number)
+	for i := range size {
+		*(*uint8)(unsafe.Add(unsafe.Pointer(&newNumber), i)) = *(*uint8)(unsafe.Add(unsafe.Pointer(&number), size-1-i))
+	}
+	return newNumber
+}
+
+func TestСonversion(t *testing.T) {
+	tests := map[string]struct {
+		number uint32
+		result uint32
+	}{
+		"test case #1": {
+			number: 0x00000000,
+			result: 0x00000000,
+		},
+		"test case #2": {
+			number: 0xFFFFFFFF,
+			result: 0xFFFFFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF00FF,
+			result: 0xFF00FF00,
+		},
+		"test case #4": {
+			number: 0x0000FFFF,
+			result: 0xFFFF0000,
+		},
+		"test case #5": {
+			number: 0x01020304,
+			result: 0x04030201,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
